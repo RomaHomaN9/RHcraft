4ce33661520bc0ee57032b6facfb4befcbc6a217
RomaHomaN9 <romahoman9@gmail.com>
2026-02-21 20:22:20 +0200

    прибрав конфлікт

--- src/version/js/selectBlocks.js
+++ src/version/js/selectBlocks.js
@@ -26,7 +26,7 @@ for (let i = 0; i < mapBlocks[0].length; i++) {
 container.appendChild(fragment);
 
 const showSelectBlockMenu = () => {
-    if (blocksInDocument.classList[1] == "hidden") blocksInDocument.classList.replace("hidden", "notHidden");
-    else blocksInDocument.classList.replace("notHidden", "hidden");
+    if (blocksInDocument.classList[1] == "hidden") blocksInDocument.classList.replace("hidden", "not_hidden");
+    else blocksInDocument.classList.replace("not_hidden", "hidden");
     console.log(1);
 };
