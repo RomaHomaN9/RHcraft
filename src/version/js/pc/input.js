7efc52ac8b314e92626a65a1df06dfd5c389b0f8
RomaHomaN9 <romahoman9@gmail.com>
2026-02-21 20:24:11 +0200

    прибрав конфлікт

--- src/version/js/pc/input.js
+++ src/version/js/pc/input.js
@@ -58,17 +58,10 @@ canvas.addEventListener("contextmenu", (e) => {
     player.setBlock(x, y, Object.keys(map.blocks)[backpack.block]);
 });
 
-<<<<<<< HEAD
-document.addEventListener("mousedown", function (e) {
-    if (e.button == 1) {
-        showSelectBlockMenu();
-    }
-=======
 document.addEventListener("wheel", (e) => {
     const velocity = e.deltaY > 0 ? 1 : -1;
 
     backpack.setBlock(velocity);
->>>>>>> dev
 });
 
 window.addEventListener("resize", () => {
