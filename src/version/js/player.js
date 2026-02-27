70f379b37caa75c06a2d7d6a5bd6f3911c15abda
RomaHomaN9 <Cod_text@ukr.net>
2026-02-27 16:00:16 +0200

    Update player.js

--- src/version/js/player.js
+++ src/version/js/player.js
@@ -269,7 +269,7 @@ const player = {
     tntBang(inputX, inputY) {
         for (let x = inputX - 2; x <= inputX + 2; x++) {
             for (let y = inputY - 2; y <= inputY + 2; y++) {
-                if ((x - inputX) * (x - inputX) + (y - inputY) * (y - inputY) <= 8) {
+                if ((x - inputX) * (x - inputX) + (y - inputY) * (y - inputY) >= 8) {
                     continue;
                 }
 
